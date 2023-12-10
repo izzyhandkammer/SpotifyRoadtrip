4bed1ef8d0ab9ec3934838c5cc4407fb48870e7b
Derek Laboy <dlaboy@bu.edu>
2023-12-10 18:35:29 -0500

    starting server

---
+++ spotifyroadtrip/backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
