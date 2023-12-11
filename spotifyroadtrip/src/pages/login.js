0762eabf3db9531e5c99460a9dfb1d2bd4b27e60
carolriady <carolriady@gmail.com>
2023-12-11 16:40:02 -0500

    updating login and signup pages

--- spotifyroadtrip/src/pages/login.js
+++ spotifyroadtrip/src/pages/login.js
@@ -1,90 +1,100 @@
-import { Link,Heading, Text, Container, Box, Flex, VStack, FormLabel, FormControl, Input, FormErrorMessage, Button, Checkbox} from '@chakra-ui/react'
-import React from 'react'
-import {Formik, Field} from "formik"
-import {auth} from '../firebase'
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Box,
+  Stack,
+  Heading,
+  Text,
+  Link,
+  Checkbox,
+  Flex,
+  useColorModeValue,
+  Container
+} from '@chakra-ui/react';
+import React from 'react';
+import { Formik, Field } from 'formik';
+import { auth } from '../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
+function Login() {
+  const navigate = useNavigate();
+  const signInUser = async (values) => {
+    const { email, password } = values;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate('/dashboard');
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
 
-function Login(){
-   const navigate = useNavigate();
-   const signInUser = async(values) =>{
-       const {email, password} = values
-       signInWithEmailAndPassword(auth, email, password)
-       .then((userCredential) => {
-           // Signed in
-           const user = userCredential.user;
-           navigate("/dashboard")
-           // ...
-       })
-       .catch((error) => {
-           const errorCode = error.code;
-           const errorMessage = error.message;
-       });
-   };
- return (
-   <Flex bg="gray.100" align="center" justify="center" h="100vh">
-   <Box bg="white" p={6} rounded="md" w={64}>
-     <Formik
-       initialValues={{
-         email: "",
-         password: "",
-         rememberMe: false
-       }}
-       onSubmit={signInUser}
-     >
-       {({ handleSubmit, errors, touched }) => (
-         <form onSubmit={handleSubmit}>
-           <VStack spacing={4} align="flex-start">
-             <FormControl>
-               <FormLabel htmlFor="email">Email Address</FormLabel>
-               <Field
-                 as={Input}
-                 id="email"
-                 name="email"
-                 type="email"
-                 variant="filled"
-               />
-             </FormControl>
-             <FormControl isInvalid={!!errors.password && touched.password}>
-               <FormLabel htmlFor="password">Password</FormLabel>
-               <Field
-                 as={Input}
-                 id="password"
-                 name="password"
-                 type="password"
-                 variant="filled"
-                 validate={(value) => {
-                   if (value.length < 6) {
-                     return "Password should be over 6 characters.";
-                   }
-                 }}
-               />
-               <FormErrorMessage>{errors.password}</FormErrorMessage>
-             </FormControl>
-             <Field
-               as={Checkbox}
-               id="rememberMe"
-               name="rememberMe"
-               colorScheme="green"
-             >
-               Remember me?
-             </Field>
-             <Button type="submit" colorScheme="green" w="full">
-               Login
-             </Button>
-             <Text>
-                 Don't have an account? Sign createUserWithEmailAndPassword{" "}
-                 <Link color="green" href="/signup">here</Link>
-             </Text>
-           </VStack>
-         </form>
-       )}
-     </Formik>
-   </Box>
- </Flex>
- )
+  const formBackground = useColorModeValue('gray.50', 'gray.700');
+
+  return (
+    <Flex align="center" justify="center" h="100vh" bgImage="url('loginBG.png')" bgSize="cover">
+      <Box bg={useColorModeValue('rgba(255, 255, 255, 0.8)', 'rgba(0, 0, 0, 0.8)')} p={6} rounded="lg" shadow="md">
+        <Heading mb={6}>Log In</Heading>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+            rememberMe: false
+          }}
+          onSubmit={signInUser}
+        >
+          {({ handleSubmit, errors, touched }) => (
+            <form onSubmit={handleSubmit}>
+              <Stack spacing={4}>
+                <FormControl isInvalid={!!errors.email && touched.email}>
+                  <FormLabel htmlFor="email">Email Address</FormLabel>
+                  <Field as={Input} id="email" name="email" type="email" variant="outline" />
+                  <FormErrorMessage>{errors.email}</FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={!!errors.password && touched.password}>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Field
+                    as={Input}
+                    id="password"
+                    name="password"
+                    type="password"
+                    variant="outline"
+                    validate={(value) => {
+                      if (value.length < 6) {
+                        return 'Password must be at least 6 characters.';
+                      }
+                    }}
+                  />
+                  <FormErrorMessage>{errors.password}</FormErrorMessage>
+                </FormControl>
+                <Field
+                  as={Checkbox}
+                  id="rememberMe"
+                  name="rememberMe"
+                >
+                  Remember Me
+                </Field>
+                <Button type="submit" colorScheme="blue" size="lg" w="full">
+                  Login
+                </Button>
+              </Stack>
+            </form>
+          )}
+        </Formik>
+        <Text align="center" mt={4}>
+          Don't have an account? <Link color="blue.500" href="/register">Register here</Link>
+        </Text>
+        </Box>
+    </Flex>
+  );
 }
 
-
-export default Login
+export default Login;
--- spotifyroadtrip/src/pages/signup.js
+++ spotifyroadtrip/src/pages/signup.js
@@ -1,74 +1,73 @@
-import { Link,Heading, Text, Container, Box, Flex, VStack, FormLabel, FormControl, Input, FormErrorMessage, Button, Checkbox} from '@chakra-ui/react'
-import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, getUserByEmail } from "firebase/auth";
-import {auth} from '../firebase'
-import React from 'react'
-import { useNavigate } from "react-router-dom";
-import {Formik, Field} from "formik"
+import {
+  Button, Input, FormControl, FormLabel, FormErrorMessage, Box, Stack, Heading, useColorModeValue, VStack, Image
+} from '@chakra-ui/react';
+import React from 'react';
+import { Formik, Field } from 'formik';
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
+function SignUp() {
+  const navigate = useNavigate();
+  const createUser = async (values) => {
+    const { email, password } = values;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((auth) => {
+        if (auth) {
+          navigate('/');
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
 
-function SignUp(){
- const navigate = useNavigate();
- const createUser = async(values) =>{
-   const {email, password} = values
-   createUserWithEmailAndPassword(auth, email, password)
-   .then((auth) => {
-       if (auth) {
-           navigate("/");
-       }
-   })
-   .catch(error => alert(error.message))
-};
- return (
-   <Flex bg="gray.100" align="center" justify="center" h="100vh">
-   <Box bg="white" p={6} rounded="md" w={64}>
-     <Formik
-       initialValues={{
-         email: "",
-         password: "",
-         rememberMe: false
-       }}
-       onSubmit={createUser}
-     >
-       {({ handleSubmit, errors, touched }) => (
-         <form onSubmit={handleSubmit}>
-           <VStack spacing={4} align="flex-start">
-             <FormControl>
-               <FormLabel htmlFor="email">Email Address</FormLabel>
-               <Field
-                 as={Input}
-                 id="email"
-                 name="email"
-                 type="email"
-                 variant="filled"
-               />
-             </FormControl>
-             <FormControl isInvalid={!!errors.password && touched.password}>
-               <FormLabel htmlFor="password">Password</FormLabel>
-               <Field
-                 as={Input}
-                 id="password"
-                 name="password"
-                 type="password"
-                 variant="filled"
-                 validate={(value) => {
-                   if (value.length < 6) {
-                     return "Password should be over 6 characters.";
-                   }
-                 }}
-               />
-               <FormErrorMessage>{errors.password}</FormErrorMessage>
-             </FormControl>
-             <Button type="submit" colorScheme="blue" w="full">
-               Sign Up
-             </Button>
-           </VStack>
-         </form>
-       )}
-     </Formik>
-   </Box>
- </Flex>
- )
+  const formBackground = useColorModeValue('gray.50', 'gray.700');
+
+  return (
+    <VStack spacing={8} align="stretch" bgImage="url('loginBG.png')" bgSize="cover" h="100vh">
+      <Image src="logo.png" alignSelf="center" boxSize="150px" mt={12} />
+      <Box bg={useColorModeValue('rgba(255, 255, 255, 0.8)', 'rgba(0, 0, 0, 0.8)')} p={6} rounded="lg" shadow="md" alignSelf="center">
+        <Heading mb={6}>Sign Up</Heading>
+        <Formik
+          initialValues={{
+            email: '',
+            password: ''
+          }}
+          onSubmit={createUser}
+        >
+          {({ handleSubmit, errors, touched }) => (
+            <form onSubmit={handleSubmit}>
+              <Stack spacing={4}>
+                <FormControl isInvalid={!!errors.email && touched.email}>
+                  <FormLabel htmlFor="email">Email Address</FormLabel>
+                  <Field as={Input} id="email" name="email" type="email" variant="outline" />
+                  <FormErrorMessage>{errors.email}</FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={!!errors.password && touched.password}>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Field
+                    as={Input}
+                    id="password"
+                    name="password"
+                    type="password"
+                    variant="outline"
+                    validate={(value) => {
+                      if (value.length < 6) {
+                        return 'Password must be at least 6 characters.';
+                      }
+                    }}
+                  />
+                  <FormErrorMessage>{errors.password}</FormErrorMessage>
+                </FormControl>
+                <Button type="submit" colorScheme="blue" size="lg" w="full">
+                  Sign Up
+                </Button>
+              </Stack>
+            </form>
+          )}
+        </Formik>
+      </Box>
+    </VStack>
+  );
 }
 
-
-export default SignUp
+export default SignUp;
