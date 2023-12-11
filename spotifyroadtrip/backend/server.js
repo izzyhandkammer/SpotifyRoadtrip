f208386edcb25cb468cb876664bbea3a2ea256e1
Derek Laboy <dlaboy@bu.edu>
2023-12-10 19:34:12 -0500

    add spotify oauth2 login endpoint with token exchange handling

--- spotifyroadtrip/backend/server.js
+++ spotifyroadtrip/backend/server.js
@@ -36,3 +36,26 @@ app.post('/spotify/refresh', (req, res) => {
         res.sendStatus(400);
     });
 });
+
+app.post('/spotify/login', (req, res) => {
+    const code = req.body.code;
+
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000/dashboard',
+        clientId: spotifyClientId,
+        clientSecret: spotifyClientSecret
+    });
+    // console.log(code);
+    spotifyApi.authorizationCodeGrant(code)
+    .then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        });
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+});
+
