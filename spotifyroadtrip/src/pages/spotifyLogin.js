8e26856f778b0a08404c8a4094c6bda649187cf4
carolriady <carolriady@gmail.com>
2023-12-11 16:59:59 -0500

    editing spotify login page

--- spotifyroadtrip/src/pages/spotifyLogin.js
+++ spotifyroadtrip/src/pages/spotifyLogin.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import {Button} from '@chakra-ui/react';
+import { Button, Box, Heading, useColorModeValue, Container, Flex, Image } from '@chakra-ui/react';
 
 const AUTH_URL = process.env.REACT_APP_SPOTIFY_AUTH_URL;
+const SpotifyIcon = 'spotify_logo.svg'; 
 
-export default function Login() {
+export default function SpotifyLogin() {
+  const formBackground = useColorModeValue('gray.50', 'gray.700');
+
   return (
-    <div className = "spotifyLogin">
-      <Button onClick={() => window.location.href = (AUTH_URL)} colorScheme="green" size="lg" >Login With Spotify</Button>
-    </div>
-  )
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Container centerContent>
+        <Box bg={formBackground} p={6} rounded="lg" shadow="md" maxWidth="sm">
+          <Heading mb={6} textAlign="center">Spotify Login</Heading>
+          <Button 
+            onClick={() => window.location.href = AUTH_URL} 
+            colorScheme="green" 
+            size="lg"
+            w="full"
+            leftIcon={<Image src={SpotifyIcon} boxSize="40px" />}
+          >
+            Login With Spotify
+          </Button>
+        </Box>
+      </Container>
+    </Flex>
+  );
 }
